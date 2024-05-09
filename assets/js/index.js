dd74628e61c7d6b222ead01fb559b732b4cdf207
spl-javmaldonado <javiera.maldonado.agurto@gmail.com>
2024-05-08 23:25:55 -0400

    commit 8

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,22 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+const myModal = document.getElementById("myModal");
+const myInput = document.getElementById("myInput");
+
+myModal.addEventListener("shown.bs.modal", () => {
+  myInput.focus();
+});
 
 
 
-  const myModal = document.getElementById('myModal')
-  const myInput = document.getElementById('myInput')
-  
-  myModal.addEventListener('shown.bs.modal', () => {
-    myInput.focus()
-  })
+$("#enviar").click(function(){
+    alert("El mensaje fue enviado correctamente.");
+  });
 
- 
+
+
